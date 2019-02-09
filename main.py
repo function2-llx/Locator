412ec0a3598ad76d37bbb94e230c3199e9ff575e
function2 <function2@qq.com>
2019-02-09 16:14:00 +0800

    sharpener

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from locator.Solver import Solver
 from locator.models.Point import Point
+from sharpener.sharpener import Sharpener
+import cv2
 
-imagePath = 'examples/test.jpg'
+image_path = 'examples/con2-2.jpg'
 
 if __name__ == "__main__":
-	solver = Solver(imagePath)
-	solver.getAllCell()
+	# solver = Solver(image_path)
+	# solver.getAllCell()
+	sharpener = Sharpener()
+	sharpener.sharpen(image_path)
 	
--- sharpener/sharpener.py
+++ sharpener/sharpener.py
@@ -1,6 +1,18 @@
+import numpy as np
+import cv2
+
 class Sharpener:
     def __init__(self):
         pass
 
-    
-    
+    def __get_sharpen_kernel(self):
+        return np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], np.float32)
+
+    def get_sharpen(self, src):
+        img = cv2.imread(src, 0)
+        kernel = self.__get_sharpen_kernel() 
+        dst = cv2.filter2D(img, -1, kernel=kernel)
+        return dst
+
+    def sharpen(self, src):
+        cv2.imwrite(src, self.get_sharpen(src))
